8968e4ed2b96d72146df97427743d53cf3f41d3e
Atis Ozols <atis.ozols@anatomynext.com>
2022-06-01 14:14:31 +0300

    some upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-    content: ["./dist/*.{html,js}"],
-    theme: {
-        extend: {},
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kanit: ["Kanit", "sans-serif"],
+      },
+      colors: {
+        purp: {
+          verydark: "#1d0224",
+          darkpale: "#38233d",
+          dark: "#5f2a6b",
+        },
+        stadions: {
+          blue: "#16556c",
+          lightblue: "#88c9f2",
+          green: "#8c862e",
+          red: "#401212",
+        },
+      },
     },
     plugins: [],
+  },
 };
