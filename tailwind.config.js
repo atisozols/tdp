1f145abe6b1a4be5a89c7f5d269db8c6986e889f
unknown <martinsozols8@gmail.com>
2022-06-01 21:41:43 +0300

    almost

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,48 @@ module.exports = {
         kanit: ["Kanit", "sans-serif"],
       },
       colors: {
-        purp: {
-          verydark: "#1d0224",
-          darkpale: "#38233d",
-          dark: "#5f2a6b",
-        },
         stadions: {
           blue: "#16556c",
           lightblue: "#88c9f2",
           green: "#8c862e",
           red: "#401212",
         },
+        kemer:{
+          red:"#F28963",
+          darkred:"#F2785C",
+          brightred:"#F2B988"
+        },
+        sloken:{
+          card:"#557544",
+          header:"#1F2517",
+          bg:"#22361B"
+        },
+        lielup:{
+          card:"#A0BED9",
+          header:"#023859",
+          bg:"#4590BF"
+        },
+        laci:{
+          card:"#B8C6D9",
+          header:"#A62139",
+          bg:"#8FB6D9"
+        },
+        zinatn:{
+          card:"#EEF4F8",
+          header:"#592246",
+          bg:"#7EA1BF"
+        },
+        oatis:{
+          card:"#F2F2F2",
+          header:"#262626",
+          bg:"#BFBFBF"
+        }
+        
       },
+      height:{
+        custom:'26rem',
+        custom1:'22rem'
+      }
     },
     plugins: [],
   },
